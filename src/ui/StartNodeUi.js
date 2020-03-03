14b63a442825a74162fb4a684ccfa506d5ea2bb1
Emmanuel Pire <piremmanuel@gmail.com>
2020-03-03 01:58:14 +0100

    use path

--- src/ui/StartNodeUi.js
+++ src/ui/StartNodeUi.js
@@ -1,4 +1,4 @@
-import { Rect, Text } from 'konva';
+import { Path, Text } from 'konva';
 import NodeUi from './NodeUi';
 
 class StartNodeUi extends NodeUi {
@@ -7,17 +7,18 @@ class StartNodeUi extends NodeUi {
     const width = 100;
     const height = 75;
     this.group.add(
-      new Rect({
+      new Path({
+        data:
+          'M4,1 L98,1 C99.6568542,1 101,2.34314575 101,4 L101,29 L101,29 L107,38 L101,47 L101,73 C101,74.6568542 99.6568542,76 98,76 L4,76 C2.34314575,76 1,74.6568542 1,73 L1,4 C1,2.34314575 2.34314575,1 4,1 Z',
         x: 0,
         y: 0,
-        width,
-        height,
+        // width,
+        // height,
         fill: '#91fbcf',
         shadowColor: 'black',
         shadowBlur: 5,
         shadowOffset: { x: 0, y: 0 },
         shadowOpacity: 0.2,
-        cornerRadius: 3,
       }),
     );
     this.group.add(
