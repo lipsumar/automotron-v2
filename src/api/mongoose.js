a55628e2127cd2cfe4f3753f021987cd26ce7db5
Emmanuel Pire <piremmanuel@gmail.com>
2020-03-15 16:11:16 +0100

    debug

--- src/api/mongoose.js
+++ src/api/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+console.log('Connectin to mongo', process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
