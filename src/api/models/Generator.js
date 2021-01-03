8ef75a2e433891f37f33f4864688bc7b163155c1
Emmanuel Pire <piremmanuel@gmail.com>
2021-01-03 16:33:01 +0100

    add timestamps

--- src/api/models/Generator.js
+++ src/api/models/Generator.js
@@ -1,15 +1,18 @@
 import mongoose from 'mongoose';
 import shortid from 'shortid';
 
-const GeneratorSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    default: shortid.generate,
+const GeneratorSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: shortid.generate,
+    },
+    graphData: String,
+    userId: String,
+    title: String,
   },
-  graphData: String,
-  userId: String,
-  title: String,
-});
+  { timestamps: true },
+);
 
 const Generator = mongoose.model('Generator', GeneratorSchema);
 
